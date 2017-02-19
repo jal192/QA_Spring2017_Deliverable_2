6fe8b70a1c737a618f5d7e1379233e78e9e301a0
Jason Ly <jal192@pitt.edu>
2017-02-18 20:13:16 -0500

    Moved functionality from citysim to here, seems to make more sense here since LocationNodeList creates and keeps track of the city. Also makes testing individual methods easier.

--- LocationNodeList.java
+++ LocationNodeList.java
@@ -4,9 +4,8 @@
 	cs1632
 	Deliverable 2
 	
-	#########################################
-	####### TODO Need to add comments #######
-	#########################################
+	This class is used to create the entire city layout and prepare it for use in the 
+	simulation. All locations are initialized and all connections are set.
 */
 
 import java.util.*;
@@ -14,14 +13,17 @@ import java.io.*;
 
 public class LocationNodeList {
 
-	ArrayList<LocationNode> lList;
+	private ArrayList<LocationNode> lList;
+	private String outsideCityPhil = "Philadelphia";
+	private String outsideCityClev = "Cleveland";
 
 	public LocationNodeList() {
 		
 		lList = new ArrayList<LocationNode>();
 		
 	}
-
+	
+	//	Initialize all 5 locations and set up the corresponding routes
 	public void initializeLocations() {
 		//	Create locations for the 4 buildings in the city and 2 locations outside the city
 		LocationNode presby = new LocationNode("Presby", 1, "Fourth Ave.", "Bill St.");
@@ -59,11 +61,70 @@ public class LocationNodeList {
 		this.lList.add(outsideCity);
 	}
 	
+	//	Set a new city list, primarily used in testing the getCityMap() method
 	public void setCityMap(ArrayList<LocationNode> newList) {
 		this.lList = newList;
 	}
 	
+	//	Retrieve the current or initialized city map
 	public ArrayList<LocationNode> getCityMap() {
 		return this.lList;
 	}
+	
+	
+	//	Print route details when traveling by avenue
+	public void printDriverTravelByAvenue(Driver d, LocationNode start, LocationNode end) {
+		//	If the location ID of the driver is not 5 it means that the driver is still in the city
+		//	Else the location ID is 5, means that the driver has left the city
+		//	Print out the route taken by the driver and the intial and end destinations
+		if(end.getLocationID() != 5) {
+			System.out.println("Driver " + d.getDriverID() + " heading from " + start.getLocationName() + " to " + 
+								end.getLocationName() + " via " + start.getAvenueName());
+		}
+		else if(end.getLocationID() == 5) {
+			System.out.println("Driver " + d.getDriverID() + " heading from " + start.getLocationName() + " to " + 
+								end.getLocationName() + " via " + start.getAvenueName());
+			
+			if(start.getLocationID() == 2) {
+				System.out.println("Driver " + d.getDriverID() + " has gone to " + outsideCityPhil + "!");
+			}
+			else if(start.getLocationID() == 3) {
+				System.out.println("Driver " + d.getDriverID() + " has gone to " + outsideCityClev + "!");
+			}
+		}
+	}
+	
+	
+	//	Print route details when traveling by street
+	public void printDriverTravelByStreet(Driver d, LocationNode start, LocationNode end) {
+		System.out.println("Driver " + d.getDriverID() + " heading from " + start.getLocationName() + " to " + 
+												end.getLocationName() + " via " + end.getStreetName());
+	}
+	
+	
+	//	Check the current starting position of the driver, if they are at sennott then increment the visits counter
+	//	Return the updated driver
+	public Driver checkStartPos(Driver d) {
+		//	Check if the user is currently at Sennott, also takes care of case where user starts at Sennott
+		if(d.getLocation() == 3) {
+			d.incrementVisitCounter();
+		}
+		
+		return d;
+	}
+	
+	
+	//	Check if the driver has left the city
+	//	If so return true
+	//	Else return false
+	public boolean checkDriverExitCity(Driver d) {
+		boolean driverLeavesCity = false;
+		
+		if(d.getLocation() == 5) {
+			driverLeavesCity = true;
+		}
+		
+		return driverLeavesCity;
+	}
+	
 }
