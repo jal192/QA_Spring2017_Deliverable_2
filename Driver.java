014c29d2fbede7acc5f6e68c2260708018e7d918
Jason Ly <jal192@pitt.edu>
2017-02-19 20:13:34 -0500

    Added missing comments

--- Driver.java
+++ Driver.java
@@ -5,28 +5,29 @@
 	Deliverable 2
 	
 	Class Description:
-		Driver object used to simulate a driver, keeping track of it's ID and location
+		Driver object used to simulate a driver, keeping track of it's ID, location and number of visits to Sennott.
 */
 
 
 public class Driver {
 	
-	private int driverID;
-	private LocationNode location;
-	private int numberVisitsSennott;
+	private int driverID;				//	Driver's ID
+	private LocationNode location;		//	Driver's current location
+	private int numberVisitsSennott;	//	Counter keeping track of the number of times the driver has visited Sennott
 	
+	//	Constructor for creating a driver
 	public Driver(int dID, LocationNode loc) {
 		this.driverID = dID;
 		this.location = loc;
 		this.numberVisitsSennott = 0;
 	}
 	
-	//	Return the driver number
+	//	Return the driver's ID
 	public int getDriverID() {
 		return this.driverID;
 	}
 	
-	//	Return the current location id of the driver
+	//	Return the current location of the driver
 	public LocationNode getLocation() {
 		return this.location;
 	}
@@ -36,7 +37,7 @@ public class Driver {
 		this.location = newLoc;
 	}
 	
-	//	Get the ccurrent number of times the driver has visited Sennott
+	//	Get the current number of times the driver has visited Sennott
 	public int getNumberSennottVisits() {
 		return this.numberVisitsSennott;
 	}
